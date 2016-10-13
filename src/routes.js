3a7efa609e470cda884b2b3fa104ca684d84af81
kkgarg <kamalkgarg@hotmail.com>
2016-10-13 09:45:19 +0530

    removed sample greet comonent

--- src/routes.js
+++ src/routes.js
@@ -4,15 +4,9 @@ import {Route, IndexRoute } from 'react-router';
 import App from './components/app';
 import PostsIndex from './components/posts_index';
 
-
-const Greeting = () => {
-  return <div>Greeting</div>;
-}
-
-
 export default (
   <Route path="/" component={App}>
       <IndexRoute component={PostsIndex} />
-      <Route path="/greet" component={Greeting} />
   </Route>
 );
+
