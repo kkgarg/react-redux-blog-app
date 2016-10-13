01f91a422b0b02bd0144c8efd9aae99242f4c568
kkgarg <kamalkgarg@hotmail.com>
2016-10-13 09:43:49 +0530

    Added two routes to simple demo

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,17 @@ import React from 'react';
 import {Route, IndexRoute } from 'react-router';
 
 import App from './components/app';
+import PostsIndex from './components/posts_index';
+
+
+const Greeting = () => {
+  return <div>Greeting</div>;
+}
+
 
 export default (
-  <Route path="/" component={App} />
+  <Route path="/" component={App}>
+      <IndexRoute component={PostsIndex} />
+      <Route path="/greet" component={Greeting} />
+  </Route>
 );
