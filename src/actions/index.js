950f892c3e16b763861b66cb4b96a45c465b2557
kkgarg <kamalkgarg@hotmail.com>
2016-10-14 05:31:34 +0530

    Create post is working good, but no validation yet

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,26 @@
 import axios from 'axios';
-export const FETCH_POSTS = 'FETCH_POSTS'; 
+export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';  
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = "?key=kdjdjskd33dd";
 
-const GET_POSTS_URL=`${ROOT_URL}/posts${API_KEY}`;
+const POSTS_URL=`${ROOT_URL}/posts${API_KEY}`;
 
 export function fetchPosts() {
-  const request = axios.get(GET_POSTS_URL);
+  const request = axios.get(POSTS_URL);
   return {
     type: FETCH_POSTS,
     payload: request
   };
+}
+
+
+export function createPost(props) {
+  const reqeust = axios.post(POSTS_URL, props);
+
+  return {
+    type: CREATE_POST,
+    payload: request
+  }
 }
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,50 @@
 import React, {Component} from 'react';
+import {reduxForm} from 'redux-form';
+import {createPost} from '../actions/index';
 
 class PostsNew extends Component {
   render() {
+
+    // Next 4 lines can be written using ES6 as
+    // const { fields: {title, categories, content}, handleSubmit} = this.props;
+    const handleSubmit = this.props.handleSubmit;   
+    const title = this.props.fields.title; 
+    const categories = this.props.fields.categories;
+    const content = this.props.fields.content;
+
     return(
       <div>
-        Create Form
+        <form onSubmit={handleSubmit(this.props.createPost)}>
+          <h3>Create a new post</h3>
+
+          <div className="form-group">
+            <label>Title</label>
+            <input type="text" className="form-control" {...title}/>
+          </div>
+
+          <div className="form-group">
+            <label>Categories</label>
+            <input type="text" className="form-control" {...categories}/>
+          </div>
+
+          <div className="form-group">
+            <label>Content</label>
+            <textarea className="form-control" {...content}/>
+          </div>          
+
+          <button type="submit" className="btn btn-primary">Submit</button>
+
+        </form>
       </div>
     );
   }
 }
 
-export default PostsNew;
+
+// connect: first arg is mapStateToProps, 2nd is mapDispatchToProps
+// reduxForm: first arg form config, 2nd is mapStateToProps, 3rd is mapDispatchToProps
+
+export default reduxForm({
+  form: 'PostsNewForm',
+  fields: ['title', 'categories', 'content']
+},null,{createPost})(PostsNew);
